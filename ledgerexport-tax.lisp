5570f295825ef5b521689ee168d7a16bfe18ce92
Andy Nagels <thereisanewway@gmail.com>
2016-08-25 15:00:39 +0200

    Added a comment and started work on the command line call.

--- ledgerexport-tax.lisp
+++ ledgerexport-tax.lisp
@@ -46,10 +46,15 @@
 ; export-to-txt:
 ; Export accounting register data to txt,
 ; for the given period.
-(defun export-to-txt (a-output-file)
-  (format t "~aExporting data to ~a...~%" +g-termprefix+ a-output-file)
-  ; TODO: how to call external application?
+(defun export-to-txt (a-argument)
+  (format t "~aExporting data to ~a...~%" +g-termprefix+ (assemble-export-name a-argument))
+  ; TODO: The below is a windows test for application calling. Remove it.
   (uiop:run-program `("C:\\Program Files (x86)\\Gow\\bin\\ls.exe" "-lh") :output t :error-output t)
+  ; TODO: if a-argument in Q1-4 then call ledger with the appropriate dates.
+  ; if a-argument in months then call ledger with -p? But what about the year?
+  ; TODO: the below with (intern a-argument), only for the months.
+  ; Also check if -p "january this year" is a valid PERIOD_EXPRESSION in ledger.
+  (uiop:run-program `("ledger -f ledger.dat" "-p \"" (intern a-argument) " this year\"" "-lh") :output t :error-output t)
   ;ledger -f ledger.dat -b "2016/06/01" -e "2016/07/01" reg | sort -n > reg_(date +%Y%m%d)_V001_btw_Q1
 )
 
@@ -63,7 +68,7 @@
     ((or
       (member (intern a-argument) +g-possible-arguments+)
       (member (intern a-argument) +g-months+))
-        (export-to-txt (assemble-export-name a-argument)))
+        (export-to-txt a-argument))
     (T (usage))))
 
 ; main
