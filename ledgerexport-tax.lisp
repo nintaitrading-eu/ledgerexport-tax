1f1bde1bf9c694c8071d74f7aa188a93e008d965
Andy Nagels <thereisanewway@gmail.com>
2016-08-30 15:17:24 +0200

    Added a comment, removed comments. Further experimentation on win32.

--- ledgerexport-tax.lisp
+++ ledgerexport-tax.lisp
@@ -40,23 +40,25 @@
 (defun export-to-txt (a-ledger-file a-argument)
   "Export accounting register data to txt, for the given period."
   (format t "~aExporting data to ~a...~%" +g-termprefix+ (assemble-export-name a-argument ".txt"))
-  ; TODO: The below is a windows test for application calling. Remove it.
-  ;(uiop:run-program `("C:\\Program Files (x86)\\Gow\\bin\\ls.exe" "-lh") :output t :error-output t)
   ; TODO: if a-argument in Q1-4 then call ledger with the appropriate dates.
   ; if a-argument in months then call ledger with -p? But what about the year?
   ; TODO: the below with (intern a-argument), only for the months.
   ; Also check if -p "january this year" is a valid PERIOD_EXPRESSION in ledger.
+  ; TODO: check if output-file exists and ask for removal
   (cond
     ((member (intern a-argument) +g-months+)
       (sb-ext:run-program +g-ledger-cmd+
         (list "-f" a-ledger-file "-p \"" a-argument " this year\" reg | sort -n > "
           (assemble-export-name a-argument ".txt")) :output *standard-output*))
     ; TODO: the below is how you use arguments. Implement that for creating the correct commands.
-    ;((member (intern a-argument) +g-months+) (sb-ext:run-program "C:\\Program Files (x86)\\Gow\\bin\\ls.exe" (list "-lh" a-argument) :output *standard-output*))
     ((member (intern a-argument) +g-quarters+)
-      (sb-ext:run-program +g-ledger-cmd+
-        (list "-f" a-ledger-file "-b TBD reg | sort -n > "
-          (assemble-export-name a-argument ".txt")) :output *standard-output*))
+      (sb-ext:run-program "C:\\Program Files (x86)\\Gow\\bin\\ls.exe"
+        (list "-lh" "|" "grep" "ledger"
+          ) :output (assemble-export-name a-argument ".txt")))
+    ;((member (intern a-argument) +g-quarters+)
+    ;  (sb-ext:run-program +g-ledger-cmd+
+    ;    (list "-f" a-ledger-file "-b TBD reg | sort -n > "
+    ;      (assemble-export-name a-argument ".txt")) :output *standard-output*))
     (T (format t "Error: Unknown argument ~a... export failed!~%" a-argument)))
   ;ledger -f ledger.dat -b "2016/06/01" -e "2016/07/01" reg | sort -n > reg_(date +%Y%m%d)_V001_btw_Q1
 )
