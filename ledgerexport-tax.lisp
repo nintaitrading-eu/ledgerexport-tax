a809978b3a04adbf7c572917cfedb65916c6d17a
Andy Nagels <thereisanewway@gmail.com>
2016-10-07 12:27:05 +0200

    Quarter to date implementation. Untested.

--- ledgerexport-tax.lisp
+++ ledgerexport-tax.lisp
@@ -129,16 +129,14 @@ given file."
         ;`(inferior-shell:pipe (ls.exe -lh) (grep ledger))) ; windows
         ;`(inferior-shell:pipe (,*g-ledger-cmd* -f ,a-ledger-file -p (concatenate 'string "\"" (string ,a-argument) " " (write-to-string ,(current-year-int)) "\"") reg) (sort -n))) ; FreeBSD
         `(inferior-shell:pipe (,*g-ledger-cmd* -f ,a-ledger-file -p ,(concatenate 'string (string a-argument) " " (write-to-string (current-year-int))) reg) (sort -n))) ; FreeBSD
-        ;`(inferior-shell:pipe (/usr/local/bin/ledger -f /home/rockwolf/doc/ledger/ledger.dat reg) (sort -n))) ; FreeBSD
     ))
     ((member a-argument *g-quarters*)
     (progn
-        ;(format t "~aExecuting command ~a -f ~a -p ~a reg | sort -n..." *g-ledger-cmd* a-ledger-file (concatenate 'string (string a-argument) " " (write-to-string current-year-int)))
-        (export-to-txt-cmd
-            (assemble-export-name a-argument ".txt")
-            ;`(inferior-shell:pipe (ls.exe -lh) (grep ledger))) ; windows
-            `(inferior-shell:pipe (/bin/ls -lh) (grep ledger))) ; FreeBSD
-            ;`(inferior-shell:pipe (*g-ledger-cmd* -f a-ledger-file ..) (grep ledger))) ; FreeBSD
+      (format t "~aExecuting command ~a -f ~a -b ~a -e ~a reg | sort -n..." *g-ledger-cmd* a-ledger-file (get-begindate-from-quarter a-argument) (get-enddate-from-quarter a-argument))
+      (export-to-txt-cmd
+        (assemble-export-name a-argument ".txt")
+        ;`(inferior-shell:pipe (ls.exe -lh) (grep ledger))) ; windows
+        `(inferior-shell:pipe (,*g-ledger-cmd* -f ,a-ledger-file -b ,(get-begindate-from-quarter a-argument) -e ,(get-enddate-from-quarter a-argument) reg) (sort -n))) ; FreeBSD
     ))
     (T (format t "~%Error: Unknown argument ~a... export failed!~%" (string a-argument))))
   (print-done)
